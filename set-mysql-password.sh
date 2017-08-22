52501f758c93a8af087050282d73aed78e77bcee
Phil Dodd <tripper54@gmail.com>
2017-08-22 10:10:22 +1000

    increase timeout waiting for mysql php

--- set-mysql-password.sh
+++ set-mysql-password.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /usr/sbin/mysqld &
-sleep 10
+sleep 20
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 echo "UPDATE mysql.user SET authentication_string = PASSWORD('root') WHERE User = 'root'; FLUSH PRIVILEGES" | mysql
 
