fdce185249b4eba7d7cc38d3e2fcb70edd2c47c8
Rodney Kordi <rodney.kordi@gmail.com>
2018-08-16 18:10:20 +1000

    Update dockerfile to fix permission issue in docker hub build

--- set-mysql-password.sh
+++ set-mysql-password.sh
@@ -3,8 +3,9 @@
 # Required to fix issue with mysqld failing to start
 # See https://github.com/docker/for-linux/issues/72
 find /var/lib/mysql -type f -exec touch {} \;
+
 /usr/sbin/mysqld &
-sleep 10
+sleep 30
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 echo "UPDATE mysql.user SET authentication_string = PASSWORD('root') WHERE User = 'root'; FLUSH PRIVILEGES" | mysql
 
