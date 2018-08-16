bf7fb45a5948d8303ec268bb78363718c0331337
Rodney Kordi <rodney.kordi@gmail.com>
2018-08-16 14:05:45 +1000

    Updated sleep to 90 seconds

--- set-mysql-password.sh
+++ set-mysql-password.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /usr/sbin/mysqld &
-sleep 60
+sleep 90
 echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 echo "UPDATE mysql.user SET authentication_string = PASSWORD('root') WHERE User = 'root'; FLUSH PRIVILEGES" | mysql
 
